e2772103fef7b3b9c7c4bd81f611d9b849113c65
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-02-05 20:47:29 -0300

    Fix bug in login functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="dark">
+    <html lang="pt-BR">
       <body className={fire.className}>{children}</body>
     </html>
   );
