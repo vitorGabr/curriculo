60974f98a7d75e0e2a61aefacf666efd4cac26fb
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-02-05 20:37:17 -0300

    Add dark mode support to HTML element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="dark">
       <body className={fire.className}>{children}</body>
     </html>
   );
